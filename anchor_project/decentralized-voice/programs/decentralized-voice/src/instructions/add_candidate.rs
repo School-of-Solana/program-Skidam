789ae2ebecc3f39a31c856b6c0648a17277f1ceb
Skidam <skidam4@gmail.com>
2025-12-22 14:06:50 +0100

    feat: add error check for  maximum number of candidate and inlcude candidate_name in candidiate account seeds

--- anchor_project/decentralized-voice/programs/decentralized-voice/src/instructions/add_candidate.rs
+++ anchor_project/decentralized-voice/programs/decentralized-voice/src/instructions/add_candidate.rs
@@ -3,6 +3,8 @@ use crate::states::*;
 use crate::errors::DecentralizedVoiceErrors;
 
 pub fn add_candidate (ctx: Context<AddCandidateContext>, candidate_name: String) -> Result<()> {
+
+
     let candidate_creator = &mut ctx.accounts.candidate_creator; 
     let candidate = &mut ctx.accounts.candidate; 
     let created_pool = &mut ctx.accounts.created_pool; 
@@ -11,9 +13,15 @@ pub fn add_candidate (ctx: Context<AddCandidateContext>, candidate_name: String)
         return Err(DecentralizedVoiceErrors::NotPoolOwner.into())
     }
 
+    if created_pool.total_candidate == created_pool.max_candidate_number {
+        return Err(DecentralizedVoiceErrors::MaximumCandidateReached.into())
+    }
+
     candidate.name = candidate_name; 
+    candidate.pool_address = created_pool.key();
     candidate.votes = 0;
     candidate.creator = candidate_creator.key();
+
 
     Ok(())
 
@@ -21,20 +29,21 @@ pub fn add_candidate (ctx: Context<AddCandidateContext>, candidate_name: String)
 
 
 #[derive(Accounts)]
+#[instruction(candidate_name:String)]
 pub struct AddCandidateContext<'info> {
     #[account(mut)]
     candidate_creator: Signer<'info>,
-
+    #[account(mut)]
+    created_pool: Account<'info, Pool>,
     #[account(
         init, 
         payer = candidate_creator,
         space = 8 + Candidate::INIT_SPACE,
-        seeds = [b"candidate", candidate_creator.key().as_ref()], 
+        seeds = [b"candidate", candidate_creator.key().as_ref(),candidate_name.as_bytes(),created_pool.key().as_ref()], 
         bump,
 
     )]
     candidate : Account<'info, Candidate>, 
-    created_pool: Account<'info, Pool>,
     system_program: Program<'info,System>,
 
 }
